1f8ea2fb3771e558473348750ef6eb067c4b5eca
tombreis <tom.b.reis@gmail.com>
2025-08-08 21:00:46 -0300

    update 080825

--- Projeto 040825/MeuPrimeiroAPP/models.py
+++ Projeto 040825/MeuPrimeiroAPP/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Planos(models.Model):
+    titulo = models.CharField(max_length=50)
+    descricao = models.TextField()
+    preco = models.DecimalField(decimal_places=2, max_digits=5)
+    banner = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.titulo
