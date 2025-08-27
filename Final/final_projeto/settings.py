088a09571b5a44219096887df4cc255976df0ead
tombreis <tom.b.reis@gmail.com>
2025-08-27 19:14:35 -0300

    Projeto Final
    ERRO !!!
    ERRO !!!
    ERRO !!!

--- Final/final_projeto/settings.py
+++ Final/final_projeto/settings.py
@@ -112,14 +112,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'final_projeto_app/static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
